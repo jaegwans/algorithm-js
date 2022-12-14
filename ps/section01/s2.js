5820f9d88f6128e4ce10b1c9c1ab321d73344bae
jaegwans <skhu201811140@gmail.com>
2022-12-14 11:46:20 +0900

    feat: 1-2

---
+++ ps/section01/s2.js
@@ -0,0 +1,15 @@
+function solution(a, b, c) {
+    let answer;
+    const arr = [a, b, c];
+    const sortArr = arr.sort((a, b) => b - a);
+
+    let result = true;
+
+    if (sortArr[0] >= sortArr[1] + sortArr[2]) {
+        result = false;
+    }
+
+    console.log(result ? 'YES' : 'NO');
+}
+
+console.log(solution(13, 33, 17));
