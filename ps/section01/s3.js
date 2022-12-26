12a07b6389af20e1d03f8f215ef314a4868bf00d
jaegwans <skhu201811140@gmail.com>
2022-12-26 11:27:42 +0900

    feat: 1-3 sol

---
+++ ps/section01/s3.js
@@ -0,0 +1,18 @@
+function solution(i) {
+    let answer;
+    let _flag = true;
+    let penQuantity = 12;
+    while (_flag) {
+        if (i > penQuantity) {
+            penQuantity += 12;
+        } else {
+            answer = penQuantity;
+            break;
+        }
+
+        console.log(penQuantity);
+    }
+    return answer / 12;
+}
+
+console.log(solution(178));
