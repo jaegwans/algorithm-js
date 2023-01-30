4a0dae19d9c3f5da2ec30b807fe51848663951c5
jaegwans <skhu201811140@gmail.com>
2023-01-30 16:29:49 +0900

    fix

--- ps/section01/s10.js
+++ ps/section01/s10.js
@@ -3,9 +3,8 @@ function solution(i) {
     let answer = i;
     let answer2 = 0;
     const second = 'R';
-    while (answer.indexOf('R') !== -1) {
-        answer = answer.replace('R', '#');
-        answer2++;
+    for (x of i) {
+        if (x === 'R') answer2++;
     }
 
     return answer2;
