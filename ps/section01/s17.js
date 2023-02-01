6ae197df06c4d6b07d2ca5cd519ffc31ec47d82f
jaegwans <skhu201811140@gmail.com>
2023-02-01 09:06:02 +0900

    fead: sol

---
+++ ps/section01/s17.js
@@ -0,0 +1,13 @@
+function solution(i) {
+    // 중복 단어 제거
+    let answer = [];
+
+    for (x of i) {
+        if (!answer.includes(x)) {
+            answer.push(x);
+        }
+    }
+    return answer;
+}
+
+console.log(solution(['5', 'good', 'time', 'good', 'time', 'student']));
